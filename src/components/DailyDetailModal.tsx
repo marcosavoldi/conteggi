989e170e95e754f6bdf4bf00902482746ce6c6ec
Marco Savoldi <marco.savo.92@gmail.com>
2026-01-08 07:54:36 +0100

    Fix DailyDetailModal chart rendering issue

--- src/components/DailyDetailModal.tsx
+++ src/components/DailyDetailModal.tsx
@@ -121,7 +121,7 @@ export const DailyDetailModal: React.FC<DailyDetailModalProps> = ({
                     </button>
                 </div>
 
-                <div style={{ flex: 1, minHeight: '300px' }}>
+                <div style={{ height: '400px', width: '100%', minHeight: '300px' }}>
                     <ResponsiveContainer width="100%" height="100%">
                         <ComposedChart data={data}>
                             <CartesianGrid strokeDasharray="3 3" opacity={0.5} />
